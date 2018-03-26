145ba362876523a9d66187ec7cc35625177933e4
Gina Benavidez <gbenavid@my.hpu.edu>
2018-03-25 19:24:48 -0700

    WIP- circle pic animations

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,17 +15,28 @@ $(document).ready(function(){
   $(document).on("click", ".clickable-on-mobile", function(e) {
     e.preventDefault();
     let src = $(this)[0].children[0].alt;
-    getRefForCircleImg(src);
+    let classNames = $(this)[0].children[0].attributes[0].nodeValue;
+    getRefForCircleImg(src, classNames);
   });
 });
 
-function callPage(page){
+function callPage(page, classNames=false){
   $.ajax({
     url: page,
     type: "GET",
     dataType: "html",
     success: function(response){
-      $(".content").html(response);
+      if (!classNames)
+        $(".content").html(response);
+      else {
+        if (!classNames.includes("active")){
+          console.log("adding class active");
+          $(classNames.split(" ").join(".")).addClass("active");
+        } else {
+          console.log("removing active class");
+          $(".circle-pic." + classNames).removeClass("active");
+        }
+      }
     },
     error: function(error) {
       console.log("the page was not loaded!", error);
@@ -33,11 +44,11 @@ function callPage(page){
   });
 };
 
-function getRefForCircleImg(alt) {
+function getRefForCircleImg(alt, classNames) {
   if (alt === "trans-american-building")
-    return callPage("./src/pages/projects.html");
+    return callPage("./src/pages/projects.html", classNames);
   if (alt === "diamond-head")
-    return callPage("./src/pages/updates.html");
+    return callPage("./src/pages/updates.html", classNames);
   if (alt === "HI")
-    return callPage("./src/pages/notes.html");
+    return callPage("./src/pages/notes.html", classNames);
 }
